a06444273aeb22b8b8eb2d0c7b0583486b0142fe
nyxfor13days <42898843+nyxfor13days@users.noreply.github.com>
2023-02-19 01:47:02 +0530

    Update components.ts

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -1,5 +1,17 @@
 import { Components, Theme } from "@mui/material";
 
-const components: Components<Omit<Theme, "components">> = {};
+const components: Components<Omit<Theme, "components">> = {
+  MuiButton: {
+    styleOverrides: {
+      root: {
+        padding: "0.375rem 20px",
+        minWidth: "72px",
+        borderRadius: "1rem",
+      },
+    },
+
+    defaultProps: { disableElevation: true },
+  },
+};
 
 export default components;
