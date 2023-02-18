6fbfa88e2c764212768fb3410267136ba3e22937
nyxfor13days <42898843+nyxfor13days@users.noreply.github.com>
2023-02-19 02:12:08 +0530

    Update typography.ts

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,7 +2,7 @@ import { TypographyOptions } from "@mui/material/styles/createTypography";
 
 const typography: TypographyOptions = {
   htmlFontSize: 16,
-  fontSize: 12,
+  fontSize: 14,
   button: {
     fontWeight: 400,
     letterSpacing: "0.05em",
